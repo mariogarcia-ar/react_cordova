190f6651260d208b095b42a6445de84a567e21bb
Mario Garcia <mariogarcia.ar@gmail.com>
2023-07-09 14:49:04 -0300

    add version 1.4.3

--- src/hooks/useUser.jsx
+++ src/hooks/useUser.jsx
@@ -7,7 +7,7 @@ import { useSystem } from "./useSystem";
 export const useUser = () =>{
     const context = useContext(UserContext);
     const {callApi} = useAxiosCustom();
-    const {getJid} = useApp();
+    const {getJid, getFordAcademyApiData} = useApp();
     const {trace} = useSystem();
 
     const getIsReady = () =>{
