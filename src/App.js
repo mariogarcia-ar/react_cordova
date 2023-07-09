029891c1fed0e07739c9368950d97637d257f7f4
Mario Garcia <mariogarcia.ar@gmail.com>
2023-07-09 19:17:01 -0300

    add version 1.5.0

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css'
 import AppContextProvider from './context'
 import AppRoutes from './pages/router'
 import Loading from "./components/loading";
+import { Toaster } from 'react-hot-toast';
 
 function App() {
 
   return (
     <div className="App">
       <AppContextProvider>
+        <Toaster />
         <Loading />
         <AppRoutes/>
       </AppContextProvider>
